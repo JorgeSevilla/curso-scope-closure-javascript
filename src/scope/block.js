f9f6724f7840f528d3fd995d32ef2d6d717ceecb
Jorge Sevilla <jorge.garcia@compasso.com.br>
2022-08-30 17:15:01 -0300

    Scope local de bloques

--- src/scope/block.js
+++ src/scope/block.js
@@ -1,3 +1,13 @@
+/*El scope local de bloque es el entorno donde las variables locales únicamente pueden ser accedidas desde un bloque de código del programa. 
+Un bloque de código es todo aquello que está dentro de los caracteres de llaves {}.
+
+Hoisting en variables
+Para hablar del scope de bloque, realizaré una breve explicacion de Hoisting. Hablaré un más de esto en su clase respectiva.
+
+Hoisting es un término para describir que la declaración de variables y funciones son desplazadas a la parte superior del scope más cercano.
+
+*/
+
 function fruits(){
     if(true){ 
 
