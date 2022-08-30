3f4e26c877604fa3f7600cb4f2c95fae9a14293b
Jorge Sevilla <solopunks803@gmail.com>
2022-08-30 17:13:33 -0300

    Scope local de bloques

---
+++ src/scope/block.js
@@ -0,0 +1,16 @@
+function fruits(){
+    if(true){ 
+
+    var fruit1 = 'Apple';
+    let fruit2 = 'Banana';
+    const fruit3 = 'Kiwi';
+
+    console.log(fruit2);
+    console.log(fruit3);
+
+    }
+
+    console.log(fruit1);    
+}
+
+fruits();
